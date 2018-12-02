bf688d1ed9b022e20dcb471ad6e649008017c884
Élie BRAMI <elie.brami@epita.fr>
2018-12-02 02:40:49 +0100

    moulinette augmenté avec pourcentage de reussite et test ajoutés

--- lexer.ml
+++ lexer.ml
@@ -1,9 +1,11 @@
-let compteur_de_test = ref 0
+let (compteur_de_test, test_valide) = ref 0, ref 0
 
 let rec test_unitaire_assert a b =
   let () =
     if a <> b then
       Printf.printf "Le test #%d n'as pas produit la valeure attendue\n" !compteur_de_test
+    else
+      test_valide := !test_valide + 1
   in compteur_de_test := !compteur_de_test + 1
 
 let est_entier t(*exte*) b(*ase*) =
@@ -55,6 +57,8 @@ let est_entier_base t(*exte*) =
         &&
           est_entier (String.sub t 0 i) b)
 
+let est_variable s = false
+
 (*
 let parenthese_correcte t(*exte*) =
   let l(*onguer*) = String.length t in
@@ -67,10 +71,13 @@ let parenthese_correcte t(*exte*) =
 *)
 
 
+;;
+
 
 let () = test_unitaire_assert (est_entier "123" 10) true
 let () = test_unitaire_assert (est_entier "-123" 10) true
 let () = test_unitaire_assert (est_entier "+123" 10) true
+let () = test_unitaire_assert (est_entier "007" 10) true
 let () = test_unitaire_assert (est_entier "0" 10) true
 let () = test_unitaire_assert (est_entier "" 10) false
 let () = test_unitaire_assert (est_entier "123a" 10) false
@@ -83,10 +90,33 @@ let () = test_unitaire_assert (est_entier_base "(123)_10") true
 let () = test_unitaire_assert (est_entier_base "123)_10") false
 let () = test_unitaire_assert (est_entier_base "(123_10") false
 let () = test_unitaire_assert (est_entier_base "_123_10") false
+let () = test_unitaire_assert (est_entier_base "g123_10") false
 let () = test_unitaire_assert (est_entier_base "123u_10") false
 let () = test_unitaire_assert (est_entier_base "12b3_10") false
+let () = test_unitaire_assert (est_entier_base "à12b3_10") false
+
+let () = test_unitaire_assert (est_variable "a") true
+let () = test_unitaire_assert (est_variable "x") true
+let () = test_unitaire_assert (est_variable "x'") true
+let () = test_unitaire_assert (est_variable "pi") true
+let () = test_unitaire_assert (est_variable "pi2") true
+let () = test_unitaire_assert (est_variable "pi-2") false
+let () = test_unitaire_assert (est_variable "pi_t") true
+let () = test_unitaire_assert (est_variable "pi_2") true
+let () = test_unitaire_assert (est_variable "a_") false
+let () = test_unitaire_assert (est_variable "Ax") false (*Constante qui commence par une maj ? *)
+let () = test_unitaire_assert (est_variable "x'''''") true
+let () = test_unitaire_assert (est_variable "x'''''123") true
+
+
 
 ;;
+let () =
+  Printf.printf "%3.2f %% of tests passed" (100. *. float_of_int !test_valide /. float_of_int !compteur_de_test) in
+let () = compteur_de_test := 0 in
+test_valide := 0
+;;
+
 
 
 
@@ -113,3 +143,4 @@ let is_int_base s =
     with Not_found -> 0
   in i != 0 && 5 <= l && s.[0] = '(' && s.[i - 2] = ')' && is_int (String.sub s 1 (l - i + 2)) (int_of_string (String.sub s i (l - i)))
 *)
+

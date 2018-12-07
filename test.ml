96f2170579e1882e3a93b41522bb6c158daa9269
Élie BRAMI <elie.brami@epita.fr>
2018-12-07 01:03:33 +0100

    moulinette augmenté avec plus de stats

--- test.ml
+++ test.ml
@@ -3,15 +3,40 @@ open Utils
 open Lexer
 open Parser
 
-let (compteur_de_test, test_valide) = ref 0, ref 0
+let (compteur_de_test, test_valide, numero_de_test, sous_test, sous_test_valide) = ref 0, ref 0, ref 0, ref 0, ref 0
+
+(*numéro de *)
+let (test_total, test_valide_total, suite, test, test_valide) = ref 0, ref 0, ref 0, ref 0, ref 0
 
 let rec test_unitaire_assert a b =
   let () =
     if a <> b then
-      Printf.printf "Le test #%d n'as pas produit la valeure attendue\n" !compteur_de_test
+      Printf.printf "Le test #%d n'as pas produit la valeure attendue\n" !test
     else
-      test_valide := !test_valide + 1
-in compteur_de_test := !compteur_de_test + 1
+      let () = test_valide := !test_valide + 1 in
+      test_valide_total := !test_valide_total + 1
+  in let () = test_total := !test_total + 1 in
+  test := !test + 1
+
+let init_suite name =
+  let () = Printf.printf "\n\n\x1b[4mSuite #%d\x1b[0m: %s\n" !suite name in
+  suite := !suite + 1
+
+let fin_suite () =
+  let () =
+    Printf.printf "%3.2f %% de tests passés pour cette suite\n" (100. *. float_of_int !test_valide /. float_of_int !test) in
+  let () = test_valide := 0 in
+  test := 0
+
+let reinit_test_suite () =
+  let () = test_total := 0 in
+  let () = test_valide_total := 0 in
+  let () = suite := 0 in
+  let () = test := 0 in
+  test_valide := 0
+
+
+let () = init_suite "est_entier"
 
 let () = test_unitaire_assert (est_entier "123" 10) true
 let () = test_unitaire_assert (est_entier "-123" 10) true
@@ -23,6 +48,10 @@ let () = test_unitaire_assert (est_entier "123a" 10) false
 let () = test_unitaire_assert (est_entier "_123" 10) false
 let () = test_unitaire_assert (est_entier "1u23" 10) false
 
+let () = fin_suite ()
+
+let () = init_suite "est_entier_base"
+
 let () = test_unitaire_assert (est_entier_base "12310") false
 let () = test_unitaire_assert (est_entier_base "123_10") true
 let () = test_unitaire_assert (est_entier_base "(123)_10") true
@@ -33,6 +62,10 @@ let () = test_unitaire_assert (est_entier_base "g123_10") false
 let () = test_unitaire_assert (est_entier_base "123u_10") false
 let () = test_unitaire_assert (est_entier_base "12b3_10") false
 let () = test_unitaire_assert (est_entier_base "à12b3_10") false
+
+let () = fin_suite ()
+
+let () = init_suite "est_variable"
 
 let () = test_unitaire_assert (est_variable "a") true
 let () = test_unitaire_assert (est_variable "x") true
@@ -47,11 +80,9 @@ let () = test_unitaire_assert (est_variable "Ax") false (*Constante qui commence
 let () = test_unitaire_assert (est_variable "x'''''") true
 let () = test_unitaire_assert (est_variable "x'''''123") true
 
+let () = fin_suite ()
 
 
-;;
-let () =
-  Printf.printf "%3.2f %% of tests passed" (100. *. float_of_int !test_valide /. float_of_int !compteur_de_test) in
-let () = compteur_de_test := 0 in
-test_valide := 0
-;;
+
+let () = Printf.printf "\n\n%3.2f %% des tests global passés\n" (100. *. float_of_int !test_valide_total /. float_of_int !test_total)
+let () = reinit_test_suite ()

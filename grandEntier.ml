612d1aea99fa89b0235f418f947b1bd4080b27bd
Et7f3 <cadeaudeelie@gmail.com>
2019-03-03 17:11:39 +0100

    Update grandEntier.ml
    fix some type syntax error and misc error but not logic error...
    (s* ee diff to see what is broken *)

--- grandEntier.ml
+++ grandEntier.ml
@@ -59,7 +59,9 @@ let sign grandentier =  if grandentier > 0
 then false
 else true;;
 
+(*
 type grandentier = (sign grandentier,convert grandentier);;
+*)
 (** grandentier est un tuple [(signe négatif, \[unité, dizaine, centaine, ...\])]
 [-123] correspond à [(true, \[3; 2; 1\])] *)
                  
@@ -82,15 +84,15 @@ let nth n l =
       nth_rec (l, n) ;;
       
 let comparer_nbr_pos ga gb = 
-	let rec cnbr ga gb i = function
+	let rec cnbr i = function
 |(ga,gb) when length ga > length gb -> -1
 |(ga,gb) when length ga < length gb -> 1
 |_ -> if i = (length ga) then 0
-else if (nth ((length ga) - i)) < (nth ((length gb) - i)) then 1
-else if (nth ((length ga) - i)) > (nth ((length gb) - i))  then -1
-else cnbr ga gb (i+1) 
+else if (nth ((length ga) - i) (* of what *)) < (nth ((length gb) - i) (* of what *)) then 1
+else if (nth ((length ga) - i) (* of what *)) > (nth ((length gb) - i) (* of what *))  then -1
+else cnbr (i+1) (ga,gb)
 in 
-cnbr ga gb 1;;
+cnbr 1 (ga,gb);;
 
 (*let comparer_nbr_neg ga gb = 
 	let rec cnbr ga gb i = function
@@ -106,10 +108,10 @@ cnbr ga gb 1;;
 
 
 
-let comparer ga gb = function
+let comparer ga gb = match ga, gb with
 | (true,_),(false,_) -> 1 
 | (false,_),(true,_) -> (-1)
-|((false,b),(false,d)) -> comparer_nbr_pos b d;;
+|((false,b),(false,d)) -> comparer_nbr_pos b d
 |((true,b),(true,d)) -> - comparer_nbr_pos b d;;
 
 (** 1 si ga < gb sinon 0 si ga = gb sinon -1 *)
@@ -134,7 +136,7 @@ let rec sous = function
            s :: sous(a,b)
        else
           (10 + d1 - d2) :: sous (r1,(bigint_sum [1] r2))
-      in sous (a,b);;
+      in sous (a,b)
   else
   let rec sous = function
       ([], r) | (r, []) -> r
@@ -153,7 +155,7 @@ let bigint_mult big n =
   else
     let rec mult n = function
         ([], 0) -> []
-      | ([], carry) -> bigint_of_int carry
+      | ([], carry) -> (*bigint_of_int *) [carry]
       | (d::r, carry) -> let res = n * d + carry in
 			 (res mod 10) :: mult n (r, res/10)
     in
@@ -175,35 +177,41 @@ let bigint_times big1 big2 =
 
 let additionner ga gb = function
 |((a,b),(c,d)) when a = c -> (a,bigint_sum b d)
+(* et les patates ? *)
 
 
 (** 1 si ga < gb sinon 0 si ga = gb sinon -1 *)
 (** renvoie ga + gb *)
 
 let soustraire ga gb = function
-|((a,b),(c,d)) when a = false and b = true -> (false,bigint_sum b d)
-|((a,b),(c,d)) when a = true and b = false -> (true,bigint_sum b d)
-|((a,b),(c,d)) when a = b -> if (comparer_nbr_pos b d) = -1 (* il verifie pour le signe a renvoyer,ici ga > gb*)
+|((a,b),(c,d)) when a = false && c = true -> (false,bigint_sum b d)
+|((a,b),(c,d)) when a = true && c = false -> (true,bigint_sum b d)
+|((a,b),(c,d)) when a = c -> if (comparer_nbr_pos b d) = -1 (* il verifie pour le signe a renvoyer,ici ga > gb*)
 							then (a,soustraction b d) 
 							else (not a, soustraction b d);;
+(* et les patates ? *)
 
 (** renvoie ga - gb *)
 
 let multiplier ga gb = function
 |((a,b),(c,d)) when a = c -> (false,bigint_times b d)
-|_ -> (true,bigint_times b d)
+|((a,b),(c,d)) -> (true,bigint_times b d)
 (** renvoie ga * gb *)
 
 let pgcd ga gb = (false, [])
 (** renvoie pgcd(ga, gb) *)
 
 let diviser_multiple ga gb = 
-	let rec d_m ga gb d = if (comparer ga (bigint_mult gb d)) =  1 then (d-1) else d_m ga gb (d+1) in d_m ga gb 1
+	let rec d_m ga gb d = if (comparer ga (true, (bigint_mult gb d))) =  1 then (d-1) else d_m ga gb (d+1) in d_m ga gb 1
 (** renvoie ga / gb où ga est multiple de gb *)
 
 (** 1 si ga < gb sinon 0 si ga = gb sinon -1 *)
 
+(*
 let diviser ga gb = let d = diviser_multiple ga gb and (a,b) = 
+et le frommage ... syntaxe error mec c'est pas complet
+*)
+
 
 (** renvoie (nominateur, dénominateur) de la fraction ga / gb *)
 
@@ -219,6 +227,7 @@ let textedechiffre ga =
 	| (_,e::ga) -> e ^ tdc (false,ga)
 	in
 	tdc ga ;;
+	(* mauvais prototype c'ets int list pas string list... *)
 (*Convertit basiquement le nombre*)
 
 let texte_depuis_grandentier ga = 

ff3ffa28972c8bdbb69c0041df364bdaa1b27b28
Élie BRAMI <elie.brami@epita.fr>
2019-03-08 17:03:18 +0100

    hide unused name (* some new button *) fix the warning and put all in the main function (to be able to be called)

--- interface.ml
+++ interface.ml
@@ -1,92 +1,94 @@
-let _ = GMain.init ()
-(*La fenêtre principale*)
+(*boucle principal de l'interface initiï¿½ par GMain.main*)
+let main (_, _) =
+
+let _ = GMain.init () in
+(*La fenï¿½tre principale*)
 let window = GWindow.window
   ~decorated:true
   ~deletable:true
   ~height:250
   ~width:500
   ~resizable:true
-  ~title:"Ocalc" ()
+  ~title:"Ocalc" () in
 
 let vbox = GPack.vbox
   ~border_width:5
-  ~packing:window#add ()
+  ~packing:window#add () in
 
 let hbox = GPack.hbox
   ~spacing:3
   ~border_width:5
-  ~packing:(vbox#pack ~expand:false) ()
-(*Création de la zone d'entrée + bouton de recherche*)  
+  ~packing:(vbox#pack ~expand:false) () in
+(*Crï¿½ation de la zone d'entrï¿½e + bouton de recherche*)
 let entry = GEdit.entry
   ~text:"Entrez votre formule"
   ~visibility:true
   ~editable:true
-  ~packing:hbox#add ()
+  ~packing:hbox#add () in
 
 let search = GButton.button
-  ~label:"Résoudre"
-  ~packing:(hbox#pack ~expand:false) ()
-(*toolbar et ses boutons*)  
+  ~label:"Rï¿½soudre"
+  ~packing:(hbox#pack ~expand:false) () in
+(*toolbar et ses boutons*)
 let toolbar = GButton.toolbar
   ~orientation:`HORIZONTAL
   ~style:`TEXT
   ~tooltips:true
-  ~packing:(hbox#pack ~expand:false) ()
+  ~packing:(hbox#pack ~expand:false) () in
 
-let toolb = GButton.tool_button
+let _ (*toolb*) = GButton.tool_button
   ~label:"Menu principale"
   ~homogeneous:true
   ~expand:false
   ~show:false
-  ~packing:(toolbar#insert ~pos:0) ()
+  ~packing:(toolbar#insert ~pos:0) () in
 
-let toolb2 = GButton.tool_button
+let _ (*toolb2*) = GButton.tool_button
   ~label:"Historique"
   ~homogeneous:true
   ~expand:false
   ~show:false
-  ~packing:(toolbar#insert ~pos:1) ()
+  ~packing:(toolbar#insert ~pos:1) () in
 
-let toolb3 = GButton.tool_button
+let _ (*toolb3*) = GButton.tool_button
   ~label:"Solveur"
   ~homogeneous:true
   ~expand:false
   ~show:false
-  ~packing:(toolbar#insert ~pos:2) ()
-  
-let toolb4 = GButton.tool_button
+  ~packing:(toolbar#insert ~pos:2) () in
+
+let _ (*toolb4*) = GButton.tool_button
   ~label:"Option"
   ~homogeneous:true
   ~expand:false
   ~show:false
-  ~packing:(toolbar#insert ~pos:3) ()
-  
+  ~packing:(toolbar#insert ~pos:3) () in
+
 let toolb5 = GButton.tool_button
   ~label:"Quitter"
   ~stock:`QUIT
   ~homogeneous:true
   ~expand:false
   ~show:true
-  ~packing:(toolbar#insert ~pos:4) ()
-(*Buffer = text qui sera affiché*)  
+  ~packing:(toolbar#insert ~pos:4) () in
+(*Buffer = text qui sera affichï¿½*)
 let res =  GText.buffer
-  ~text:"Solution :" ()
-(*Permet d'afficher le buffer*)  
-let texte = GText.view
+  ~text:"Solution :" () in
+(*Permet d'afficher le buffer*)
+let _ (*texte*) = GText.view
   ~buffer:res
   ~editable:false
   ~cursor_visible:false
   ~accepts_tab:false
   ~border_width:2
-  ~packing:(vbox#pack ~expand:true) ()
-(*boucle principal de l'interface initié par GMain.main*)
-let _ =
+  ~packing:(vbox#pack ~expand:true) () in
+
   (*pour fermer le logicielle*)
-  window#connect#destroy ~callback:GMain.quit;
-  toolb5#connect#clicked ~callback:GMain.quit;
-  (*récupartion de l'entrée avec affichage du résultat*)
-  search#connect#clicked ~callback:(fun () -> res#set_text ("Solution :\n"^entry#text));
-  (*montre la fenêtre*)
+  let _ = window#connect#destroy ~callback:GMain.quit in
+  let _ = toolb5#connect#clicked ~callback:GMain.quit in
+  (*rï¿½cupartion de l'entrï¿½e avec affichage du rï¿½sultat*)
+  let _ = search#connect#clicked ~callback:(fun () -> res#set_text ("Solution :\n"^entry#text)) in
+  (*montre la fenï¿½tre*)
   window#show ();
   (*initie la boucle principale*)
   GMain.main ()

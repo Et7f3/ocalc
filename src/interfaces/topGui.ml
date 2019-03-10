63ecb671a02d175a6953a368a97d6ffa439b5b75
Élie BRAMI <elie.brami@epita.fr>
2019-03-10 01:03:00 +0100

    fix charset and exit from main function

--- src/interfaces/topGui.ml
+++ src/interfaces/topGui.ml
@@ -1,101 +1,101 @@
 open Parser
 open Moteur
 
-(*boucle principal de l'interface initi� par GMain.main*)
-let () =
-  let _ = GMain.init () in
-  (*La fen�tre principale*)
-  let window = GWindow.window
+let _ = GMain.init ()
+
+(*La fenêtre principale*)
+let window = GWindow.window
     ~decorated:true
     ~deletable:true
     ~height:250
     ~width:500
     ~resizable:true
-    ~title:"Ocalc" () in
+    ~title:"Ocalc" ()
 
-  let vbox = GPack.vbox
+let vbox = GPack.vbox
     ~border_width:5
-    ~packing:window#add () in
+    ~packing:window#add ()
 
-  let hbox = GPack.hbox
+let hbox = GPack.hbox
     ~spacing:3
     ~border_width:5
-    ~packing:(vbox#pack ~expand:false) () in
+    ~packing:(vbox#pack ~expand:false) ()
 
-  (*Cr�ation de la zone d'entr�e + bouton de recherche*)
-  let entry = GEdit.entry
+(*Création de la zone d'entrée + bouton de recherche*)
+let entry = GEdit.entry
     ~text:"Entrez votre formule"
     ~visibility:true
     ~editable:true
-    ~packing:hbox#add () in
+    ~packing:hbox#add ()
 
-  let search = GButton.button
-    ~label:"R�soudre"
-    ~packing:(hbox#pack ~expand:false) () in
+let search = GButton.button
+    ~label:"Résoudre"
+    ~packing:(hbox#pack ~expand:false) ()
 
-  (*toolbar et ses boutons*)
-  let toolbar = GButton.toolbar
+(*toolbar et ses boutons*)
+let toolbar = GButton.toolbar
     ~orientation:`HORIZONTAL
     ~style:`TEXT
     ~tooltips:true
-    ~packing:(hbox#pack ~expand:false) () in
+    ~packing:(hbox#pack ~expand:false) ()
 
-  let _ (*toolb*) = GButton.tool_button
+let _ (*toolb*) = GButton.tool_button
     ~label:"Menu principale"
     ~homogeneous:true
     ~expand:false
     ~show:false
-    ~packing:(toolbar#insert ~pos:0) () in
+    ~packing:(toolbar#insert ~pos:0) ()
 
-  let _ (*toolb2*) = GButton.tool_button
+let _ (*toolb2*) = GButton.tool_button
     ~label:"Historique"
     ~homogeneous:true
     ~expand:false
     ~show:false
-    ~packing:(toolbar#insert ~pos:1) () in
+    ~packing:(toolbar#insert ~pos:1) ()
 
-  let _ (*toolb3*) = GButton.tool_button
+let _ (*toolb3*) = GButton.tool_button
     ~label:"Solveur"
     ~homogeneous:true
     ~expand:false
     ~show:false
-    ~packing:(toolbar#insert ~pos:2) () in
+    ~packing:(toolbar#insert ~pos:2) ()
 
-  let _ (*toolb4*) = GButton.tool_button
+let _ (*toolb4*) = GButton.tool_button
     ~label:"Option"
     ~homogeneous:true
     ~expand:false
     ~show:false
-    ~packing:(toolbar#insert ~pos:3) () in
+    ~packing:(toolbar#insert ~pos:3) ()
 
-  let toolb5 = GButton.tool_button
+let toolb5 = GButton.tool_button
     ~label:"Quitter"
     ~stock:`QUIT
     ~homogeneous:true
     ~expand:false
     ~show:true
-    ~packing:(toolbar#insert ~pos:4) () in
+    ~packing:(toolbar#insert ~pos:4) ()
 
-  (*Buffer = text qui sera affich�*)
-  let res =  GText.buffer
-    ~text:"Solution :" () in
+(*Buffer = text qui sera affiché*)
+let res =  GText.buffer
+    ~text:"Résultat:" ()
 
-  (*Permet d'afficher le buffer*)
-  let _ (*texte*) = GText.view
+(*Permet d'afficher le buffer*)
+let _ (*texte*) = GText.view
     ~buffer:res
     ~editable:false
     ~cursor_visible:false
     ~accepts_tab:false
     ~border_width:2
-    ~packing:(vbox#pack ~expand:true) () in
+    ~packing:(vbox#pack ~expand:true) ()
 
+(*boucle principal de l'interface initié par GMain.main*)
+let _ =
   (*pour fermer le logicielle*)
   let _ = window#connect#destroy ~callback:GMain.quit in
   let _ = toolb5#connect#clicked ~callback:GMain.quit in
-
-  (*r�cupartion de l'entr�e avec affichage du r�sultat*)
-  let _ = search#connect#clicked ~callback:(fun () -> res#set_text ("Solution :\n"^ (texte_de_expr (eval (expr_de_texte entry#text))))) in
-  (*montre la fen�tre*)
+  (*récupartion de l'entrée avec affichage du résultat*)
+  let _ = search#connect#clicked ~callback:(fun () -> res#set_text ("Résultat:\n" ^ (texte_de_expr (eval (expr_de_texte entry#text))))) in
+  (*montre la fenêtre*)
   window#show ();
   (*initie la boucle principale*)
   GMain.main ()

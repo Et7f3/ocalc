fca4b36db7af7453dbb67f0c07e910d332b24e09
Élie BRAMI <elie.brami@epita.fr>
2019-03-13 21:49:36 +0100

    forward argument from command line

--- compile.bat
+++ compile.bat
@@ -2,4 +2,4 @@
 
 @if not defined %OCAMLLIB% (set OCAMLLIB=%cd%\deps\lib\ocaml) else (set OCAMLLIB=%cd%\deps\lib\ocaml;%OCAMLLIB%)
 
-@.\deps\bin\ocaml.exe unix.cma compile.ml nettoyer construire
+@.\deps\bin\ocaml.exe unix.cma compile.ml nettoyer construire -- %*

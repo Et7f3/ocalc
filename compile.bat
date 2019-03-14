9db251609f56524794de1458a223b39070f56bac
Élie BRAMI <elie.brami@epita.fr>
2019-03-14 01:31:15 +0100

    patch of first defence

--- compile.bat
+++ compile.bat
@@ -1,5 +1,3 @@
 @set PATH=%cd%\deps\bin;%PATH%
-
 @if not defined %OCAMLLIB% (set OCAMLLIB=%cd%\deps\lib\ocaml) else (set OCAMLLIB=%cd%\deps\lib\ocaml;%OCAMLLIB%)
-
-@.\deps\bin\ocaml.exe unix.cma compile.ml nettoyer construire -- %*
+@.\deps\bin\ocaml.exe unix.cma compile.ml nettoyer construire -topTest -grandEntier %*

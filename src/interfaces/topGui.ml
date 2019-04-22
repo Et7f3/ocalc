232b9fcef5de4dc291955a2c7a9bb0c5ba64fb36
Élie BRAMI <elie.brami@epita.fr>
2019-04-22 22:33:11 +0200

    Basic Gui with Revery

--- src/interfaces/topGui.ml
+++ src/interfaces/topGui.ml
@@ -2,23 +2,57 @@ open Revery
 open Revery.UI
 open Revery.UI.Components
 
+type main_view_state =
+{
+  valeur: string;
+}
+
+module VuePrincipal = struct
+  let component =
+    React.component "VuePrincipal"
+
+  type action =
+    Vider
+  | MiseAJour of string
+  | Calculer
+
+  let reducer action etat =
+    match action with
+      Vider -> {valeur = ""}
+    | MiseAJour valeur -> {valeur} (* ici on met à jour notre état *)
+    | Calculer -> (* on doit calculer ici et vider l'entre
+    BoNuS: si c'est valide: tapez élie en attendant *) {valeur = ""}
+
+  let createElement =
+    let containerStyle =
+      Style.[
+        position `Absolute;
+        justifyContent `Center;
+        alignItems `Center;
+        bottom 0;
+        top 0;
+        left 0;
+        right 0
+        ]
+    in
+    fun ~children:_ () ->
+      component
+        (fun hooks  ->
+          let ({valeur}, dispatch,hooks) =
+            React.Hooks.reducer ~initialState:{valeur = ""} reducer hooks
+          in
+          (hooks, View.createElement ~style:containerStyle ~children:[
+            Input.createElement ~value:valeur ~placeholder:"Entrer votre équation" ~onChange:(fun {value; _} -> dispatch(MiseAJour value)) ~children:[] ();
+            Button.createElement ~title:"Calculer" ~onClick:(fun _ -> dispatch Calculer) ~children:[] ();
+            Button.createElement ~title:"Éffacer" ~onClick:(fun _ -> dispatch Vider) ~children:[] ()
+            ] ()))
+end
+
 let init app =
-  let win =
-  App.createWindow app (("OCalc")[@reason.raw_literal "OCalc"])
+  let fen =
+  App.createWindow app "OCalc"
   in
-  let containerStyle =
-    Style.[
-      position `Absolute;
-      justifyContent `Center;
-      alignItems `Center;
-      bottom 0;
-      top 0;
-      left 0;
-      right 0
-      ]
-  in
-  let button = Button.createElement ~onClick:(fun _ -> print_endline "cliked") ~title:"recherchez" ~children:[] () in
-  let render () = View.createElement ~style:containerStyle ~children:[button] () in
-  UI.start win render
+  let afficher () = VuePrincipal.createElement ~children:[] () in
+  UI.start fen (afficher ()) (afficher ()) (* MAJ: je ne sai spas pk il faut le mettre 2 fois *)
 
 let _ = App.start init

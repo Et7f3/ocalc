bc3792b4c64ab0eecc34277a8a4e11b00f95c2f7
Élie BRAMI <elie.brami@epita.fr>
2019-04-25 17:39:36 +0200

    renew GUI

--- src/interfaces/topGui.ml
+++ src/interfaces/topGui.ml
@@ -2,21 +2,62 @@ open Revery
 open Revery.UI
 open Revery.UI.Components
 
-type main_view_state =
+type vue =
+  [
+    `VueEquation
+  ]
+
+(*
+type vue_par_defaut_state =
+{
+  nothing : unit;
+}
+*)
+type equation_state =
 {
   valeur: string;
   context: Noyau.Moteur.context;
 }
-let _ = Commune.argc
 
-module VuePrincipal = struct
-  let component =
-    React.component "VuePrincipal"
+type application_state =
+{
+  vue_courante: vue;
+}
+
+type application_sauvegarde =
+{
+  equation: equation_state;
+}
+
+(*
+module VueParDefaut = struct
+  let component = React.component "VueParDefaut"
+
+  type action = Nope
+
+  let reducer action etat =
+    match action with
+      Nope -> etat
+
+  let createElement ~initialState ~changerVue ~onUpdate =
+  fun ~children:_ () ->
+    component
+      (fun hooks  ->
+        let ({_} (* nouvel etat *) as etat, _ (* dispatch *), hooks) =
+          React.Hooks.reducer ~initialState:{nothing = ()} reducer hooks
+        in let () = onUpdate (`VueParDefaut etat) in
+        (hooks, View.createElement ~children:[] ()))
+end
+*)
+
+
+module Equation = struct
+  let component = React.component "Equation"
 
   type action =
-    Vider
-  | MiseAJour of string
-  | Calculer
+      Vider
+    | MiseAJour of string
+    | Calculer
 
   let reducer action etat =
     match action with
@@ -25,31 +66,32 @@ module VuePrincipal = struct
     | Calculer -> (* on doit calculer ici et vider l'entre
     BoNuS: si c'est valide: tapez élie en attendant *) {etat with valeur = ""}
 
-  let createElement =
-    let containerStyle =
-      Style.[
-        position `Absolute;
-        justifyContent `Center;
-        alignItems `Center;
-        bottom 0;
-        top 0;
-        left 0;
-        right 0
-        ]
-    in
-    let textStyle =
-      Style.[
-      fontSize 25;
-      fontFamily "Roboto-Regular.ttf"
+  let createElement ~initialState ~changerVue:_ ~onUpdate =
+  let containerStyle =
+    Style.[
+      position `Absolute;
+      justifyContent `Center;
+      alignItems `Center;
+      bottom 0;
+      top 0;
+      left 0;
+      right 0
       ]
-    in
-    fun ~children:_ () ->
-      component
-        (fun hooks  ->
-          let ({valeur; _}, dispatch,hooks) =
-            React.Hooks.reducer ~initialState:{valeur = ""; context = Noyau.Moteur.empty_context} reducer hooks
-          in
-          (hooks, View.createElement ~style:containerStyle ~children:[
+  in
+  let textStyle =
+    Style.[
+    fontSize 25;
+    fontFamily "Roboto-Regular.ttf"
+    ]
+  in
+  fun ~children:_ () ->
+    component
+      (fun hooks  ->
+        let ({valeur; _} as etat, dispatch , hooks) =
+          React.Hooks.reducer ~initialState reducer hooks
+        let () = onUpdate (`Equation etat) in
+        (hooks,
+          View.createElement ~style:containerStyle ~children:[
             Input.createElement ~value:valeur ~placeholder:"Entrer votre équation" ~onChange:(fun {value; _} -> dispatch(MiseAJour value)) ~children:[] ();
             Button.createElement ~title:"Calculer" ~width:150 ~height:50 ~fontSize:25 ~onClick:(fun _ -> dispatch Calculer) ~children:[] ();
             Button.createElement ~title:"Éffacer" ~width:150 ~height:50 ~fontSize:25 ~onClick:(fun _ -> dispatch Vider) ~children:[] ();
@@ -57,136 +99,38 @@ module VuePrincipal = struct
             ] ()))
 end
 
-module History = struct
-  let component =
-    React.component "Historique"
-  let createElement ?(hyst = [""]) =
-    let containerStyle =
-      Style.[
-        position `Absolute;
-        justifyContent `Center;
-        alignItems `Center;
-        bottom 0;
-        top 0;
-        left 0;
-        right 0
-        ]
-    in
-      let textStyle =
-      Style.[
-      fontSize 25;
-      fontFamily "Roboto-Regular.ttf"
-      ]
-    in
-    let rec histol hist res =
-      match hist with
-       [] -> []
-      |e::l -> histol l ((Text.createElement ~text:e ~style:textStyle ~children:[]) :: res)
-    in
+module Application = struct
+  let component = React.component "Application"
+
+  type action =
+      ChangerVue of vue
+
+  let sauvegarde = ref {
+    equation = {valeur = "ddd"; context = Noyau.Moteur.empty_context}
+  }
+
+  let reducer action etat =
+    match action with
+      ChangerVue v -> {etat with vue_courante = v}
+
+  let miseAJour = function
+    `Equation e -> sauvegarde := {!sauvegarde with equation = e}
+
+  let createElement =
     fun ~children:_ () ->
       component
-        (fun hooks ->
-          hooks, View.createElement ~style:containerStyle ~children:(histol hyst []) ())
+        (fun hooks  ->
+          let ({vue_courante}, dispatch, hooks) =
+            React.Hooks.reducer ~initialState:{vue_courante = `VueEquation} reducer hooks
+          in let choisir_vue = function
+            `VueEquation -> Equation.createElement ~initialState:(!sauvegarde.equation)
+          in hooks, (choisir_vue vue_courante)  ~changerVue:(fun v -> dispatch (ChangerVue v)) ~onUpdate:miseAJour ~children:[] ())
 end
 
-type view =
-  {
-  name: string;
-  render: Window.t -> React.syntheticElement;
-  }
-
-type state = {
-  views: view list;
-  selectedView: string;}
-
-let state: state =
-  {
-    views =
-      [{
-         name = "Équation";
-         render = ((fun _w  -> VuePrincipal.createElement ~children:[] ()));
-       };
-       {
-         name = "Historique";
-         render = ((fun _w -> History.createElement ~children:[] ()));
-       }];
-    selectedView = "Équation"
-  }
-
-let getViewByName (state : state) (view : string) =
-  (List.filter (fun x  -> String.equal x.name view) state.views) |>
-    List.hd
-
-let getRenderFunctionSelector: state -> Window.t -> React.syntheticElement =
-  fun (s : state)  ->
-    (getViewByName s s.selectedView) |> (fun a  -> a.render)
-
-type action =
-  | SelectView of string
-
-let reducer (action : action) (state : state) =
-  match action with
-  | ((SelectView (name))[@explicit_arity ]) -> { state with selectedView = name }
-
-module Affichage = struct
-  let component = React.component "Affichage"
-  let createElement ~children:_ ~win () =
-  component
-  (fun hooks  ->
-     let (state,dispatch,hooks) =
-       React.Hooks.reducer ~initialState:state reducer hooks in
-     let renderButton (x : view) =
-       ((Button.createElement ~title:(x.name)
-            ~width:175
-            ~fontSize:25
-            ~onClick:(fun _  ->
-                       dispatch
-                         ((SelectView ((x.name)))[@explicit_arity ]))
-            ~children:[] ())[@JSX ]) in
-     let buttons = List.map renderButton state.views in
-     let viewRender = getRenderFunctionSelector state in
-     let view = viewRender win in
-     let bgColor = Color.hex("#212733") in
-     let activeBackgroundColor = Color.hex("#2E3440") in
-     (hooks,
-       ((View.createElement ~onMouseWheel:(fun _evt  -> ())
-     ~style:(let open Style in
-               [position `Absolute;
-               justifyContent `Center;
-               alignItems `Center;
-               backgroundColor bgColor;
-               bottom 0;
-               top 0;
-               left 0;
-               right 0;
-               flexDirection `Row])
-     ~children:[((ScrollView.createElement
-                    ~style:(let open Style in
-                              [position `Absolute;
-                              top 0;
-                              left 0;
-                              width 175;
-                              bottom 0;
-                              backgroundColor bgColor])
-                    ~children:[((View.createElement ~children:buttons ())
-                              [@JSX ])] ())[@JSX ]);
-               ((View.createElement
-                   ~style:(let open Style in
-                             [position `Absolute;
-                             top 0;
-                             left 175;
-                             right 0;
-                             bottom 0;
-                             backgroundColor activeBackgroundColor])
-                   ~children:[view] ())[@JSX ])] ())[@JSX ])))
-  end
-
 let init app =
-  let fen =
-  App.createWindow ~createOptions:(WindowCreateOptions.create ~icon:((Some ("logo.png"))[@explicit_arity ]) ()) app "OCalc"
-  in
-
-  let afficher () = Affichage.createElement ~win:fen ~children:[] () in
+  let options_fen = WindowCreateOptions.create ~icon:(Some "logo.png") () in
+  let fen = App.createWindow ~createOptions:options_fen app "OCalc" in
+  let afficher () = Application.createElement ~children:[] () in
   UI.start fen (afficher ()) (afficher ())
 
 let _ = App.start init

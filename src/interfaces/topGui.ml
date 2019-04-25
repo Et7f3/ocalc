f3a6d18431bc2a598f84d610c10059626fa8e998
Élie BRAMI <elie.brami@epita.fr>
2019-04-25 20:22:42 +0200

    copy past history module from quentin and bind it to core

--- src/interfaces/topGui.ml
+++ src/interfaces/topGui.ml
@@ -4,7 +4,8 @@ open Revery.UI.Components
 
 type vue =
   [
-    `VueEquation
+      `VueEquation
+    | `VueHistorique
   ]
 
 (*
@@ -17,7 +18,13 @@ type equation_state =
 {
   valeur: string;
   res: string;
+  liste_historique: string list;
   context: Noyau.Moteur.context;
+}
+
+type historique_state =
+{
+  mutable liste : string list;
 }
 
 type application_state =
@@ -28,6 +35,7 @@ type application_state =
 type application_sauvegarde =
 {
   equation: equation_state;
+  historique: historique_state;
 }
 
 (*
@@ -69,9 +77,9 @@ module Equation = struct
         try
           Noyau.Moteur.evaluate_with_history etat.valeur etat.context
         with Failure msg -> msg, etat.context (* old context *)
-      in {context = cxt; valeur = ""; res}
+      in {liste_historique = res :: etat.liste_historique; context = cxt; valeur = ""; res}
 
-  let createElement ~initialState ~changerVue:_ ~onUpdate =
+  let createElement ~initialState ~changerVue ~onUpdate =
   let containerStyle =
     Style.[
       position `Absolute;
@@ -100,8 +108,52 @@ module Equation = struct
             Input.createElement ~value:valeur ~placeholder:"Entrer votre équation" ~onChange:(fun {value; _} -> dispatch(MiseAJour value)) ~children:[] ();
             Text.createElement ~text:res(*retour du moteur*) ~style:textStyle ~children:[] ();
             Button.createElement ~title:"Calculer" ~width:150 ~height:50 ~fontSize:25 ~onClick:(fun _ -> dispatch Calculer) ~children:[] ();
-            Button.createElement ~title:"Éffacer" ~width:150 ~height:50 ~fontSize:25 ~onClick:(fun _ -> dispatch Vider) ~children:[] ()
+            Button.createElement ~title:"Éffacer" ~width:150 ~height:50 ~fontSize:25 ~onClick:(fun _ -> dispatch Vider) ~children:[] ();
+            Button.createElement ~title:"Accéder à l'historique"
+                 ~width:175
+                 ~fontSize:25
+                 ~onClick:(fun _  ->
+                            changerVue `VueHistorique)
+                 ~children:[] ()
             ] ()))
+end
+
+module Historique = struct
+  let component =
+    React.component "Historique"
+  let createElement historique ~changerVue ~onUpdate:_ =
+    let containerStyle =
+      Style.[
+        position `Absolute;
+        justifyContent `Center;
+        alignItems `Center;
+        bottom 0;
+        top 0;
+        left 0;
+        right 0
+        ]
+    in
+      let textStyle =
+      Style.[
+      fontSize 25;
+      fontFamily "Roboto-Regular.ttf"
+      ]
+    in
+    let rec histol hist res =
+      match hist with
+       [] -> []
+      | e :: l -> histol l ((Text.createElement ~text:e ~style:textStyle ~children:[]) :: res)
+    in
+    fun ~children:_ () ->
+      component
+        (fun hooks ->
+          let bouton_retour = Button.createElement ~title:"Revenir au mode Équation"
+               ~width:175
+               ~fontSize:25
+               ~onClick:(fun _  ->
+                          changerVue `VueEquation)
+               ~children:[] () in
+          hooks, View.createElement ~style:containerStyle ~children:(bouton_retour :: (histol historique [])) ())
 end
 
 module Application = struct
@@ -114,7 +166,12 @@ module Application = struct
     equation = {
       valeur = "";
       res = " "(* trick to be displayed *);
-      context = Noyau.Moteur.empty_context}
+      liste_historique = []; (* TODO: avoid this double list *)
+      context = Noyau.Moteur.empty_context
+    };
+    historique = {
+      liste = [];
+    }
   }
 
   let reducer action etat =
@@ -122,7 +179,9 @@ module Application = struct
       ChangerVue v -> {etat with vue_courante = v}
 
   let miseAJour = function
-    `Equation e -> sauvegarde := {!sauvegarde with equation = e}
+    `Equation e ->
+      let () = sauvegarde := {!sauvegarde with equation = e} in
+      !sauvegarde.historique.liste <- e.liste_historique
 
   let createElement =
     fun ~children:_ () ->
@@ -131,7 +190,8 @@ module Application = struct
           let ({vue_courante}, dispatch, hooks) =
             React.Hooks.reducer ~initialState:{vue_courante = `VueEquation} reducer hooks
           in let choisir_vue = function
-            `VueEquation -> Equation.createElement ~initialState:(!sauvegarde.equation)
+              `VueEquation -> Equation.createElement ~initialState:(!sauvegarde.equation)
+            | `VueHistorique -> Historique.createElement !sauvegarde.equation.liste_historique
           in hooks, (choisir_vue vue_courante) ~changerVue:(fun v -> dispatch (ChangerVue v)) ~onUpdate:miseAJour ~children:[] ())
 end
 

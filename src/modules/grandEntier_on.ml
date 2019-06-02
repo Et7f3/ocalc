0af6eb464f67c02acec01f1159aba4046f26eebd
Élie BRAMI <elie.brami@epita.fr>
2019-06-02 21:47:46 +0200

    merge grandEntier.ml to grandEntier_on.ml

--- src/modules/grandEntier_on.ml
+++ src/modules/grandEntier_on.ml
@@ -15,48 +15,39 @@ let neg (a, b) =
     [] -> false, []
   | _ -> not a, b
 
-let rec length = function
-    [] -> 0
-  | _ :: l -> 1 + length l
-
-(* Prend un element dans une liste *)
-let nth n l =
-  if n < 0 then
-    invalid_arg "nth: index must be a natural"
-  else
-    let rec nth_rec = function
-        [], _ -> failwith "nth: list is too short"
-      | e :: _, 0 -> e
-      | _ :: l, n -> nth_rec (l, n - 1)
-    in nth_rec (l, n)
+let nettoyer_zero a =
+  let rec n0 = function
+      [] -> []
+    | 0 :: c -> n0 c
+    | c -> List.rev c
+  in n0 (List.rev a)
 
 (** 1 si ga < gb sinon 0 si ga = gb sinon -1 *)
 (* 1 : gb > ga
    0 : ga = gb
   -1 : ga > gb*)
 
-
-(* TODO: erase + 1 *)
-
 let comparer_nbr_abs ga gb =
-  let la = length ga
-  and lb  = length gb in
-  (* Compare un a un les elements en partant de la fin *)
+  let la = List.length ga
+  and lb = List.length gb in
   if la > lb then
     -1
   else if la < lb then
    1
   else
-    let rec cna i =
-      if i = la + 1 then
-        0
-      else if nth (la - i) ga < (nth (lb - i) gb) then
-        1
-      else if nth (la - i) ga > (nth (lb - i) gb) then
-        -1
-      else
-        cna (i + 1)
-    in cna 1
+    let rec cna = function
+        e1 :: ga, e2 :: gb ->
+        if e1 > e2 then
+          -1
+        else if e2 > e1 then
+          1
+        else
+          cna (ga, gb)
+      (* this will not happen *)
+      | [], [] -> 0
+      | _, [] -> -1
+      | [], _ -> 1
+    in cna (List.rev ga, List.rev gb)
 
 let comparer ga gb =
   match ga, gb with
@@ -80,13 +71,13 @@ let bigint_sum big1 big2 =
   in add (big1, big2)
 
 (* Constructeur propre a la sousatraction qui va
-permettre de ne pas effectuer la retenue*)
+permettre de ne pas effectuer la retenue *)
 let bigint_sum_for_sous big1 big2 =
   let rec add = function
       [], r | r, [] -> r
     | d1 :: r1, d2 :: r2 ->
       let s = d1 + d2 in
-      s :: add(r1, r2)
+      s :: add (r1, r2)
   in add (big1, big2)
 
 (* On preferera a > b en abs et on aura toujours*)
@@ -98,6 +89,8 @@ let rec sous = function
       (d1 - d2) :: sous(r1, r2)
     else
       (d1 + 10 - d2) :: sous (r1, (bigint_sum_for_sous [1] r2))
+
+let sous a = sous a |> nettoyer_zero
 
 (** renvoie ga + gb *)
 let additionner ga gb =
@@ -126,14 +119,16 @@ let bigint_mult big n =
   else
     let rec mult n = function
         [], 0 -> []
-      | [], carry -> (*bigint_of_int *) [carry]
+      | [], carry -> (* bigint_of_int *) [carry]
       | d :: r, carry ->
         let res = n * d + carry in
-        (res mod 10) :: mult n (r, res/10)
-    in match n with
+          (res mod 10) :: mult n (r, res / 10)
+    in
+    match n with
       0 -> []
     | 1 -> big
     | n -> mult n (big, 0)
+
 
 let bigint_times big1 big2 =
   let rec mult = function
@@ -150,33 +145,58 @@ let multiplier ga gb =
     (a, b), (c, d) when a = c -> false, bigint_times b d
   | (_, b), (_, d) -> true, bigint_times b d
 
-(** renvoie pgcd(ga, gb) *)
-let pgcd _ _ = (false, [])
-
-let diviser_multiple_abs a b =
-  let rec d_m a b e =
-    if (comparer_nbr_abs a (bigint_times b e)) >= 0 then
-      e
+let abaisser a e ret =
+  let rec abaisser a n =
+    let cmp = comparer_nbr_abs a e in
+    if cmp = 1 then
+      a, n
     else
-      d_m a b (bigint_sum e [1])
-  in d_m a b [1]
+      abaisser (sous (a, e)) (n + 1)
+  in let a, n = abaisser a 0 in
+  a, n :: ret
+
+let rec div_mul a e =
+  let cmp = comparer_nbr_abs a e in
+  if cmp = 1 then
+    a, []
+  else
+    let a, n = div_mul a (0 :: e) in
+    abaisser a e n
+
+let div_eucl = function
+    _, (_, []) -> raise Division_by_zero
+  | (a, b), (c, d) ->
+    let m, div = div_mul b d in
+    (a, m), (a <> c, div)
+
+
+let div_eucl_fix = function
+    _, (_, []) -> raise Division_by_zero
+  | (false as a, b), (c, d) ->
+    let m, div = div_mul b d in
+    (a, m), (a <> c, div)
+  | (true as a, b), (c, d) ->
+    let m, div = div_mul b d in
+    additionner (a, m) (false, d), additionner (a <> c, div) (not c, [1])
 
 (** renvoie ga / gb où ga est multiple de gb *)
-let diviser_multiple ga gb =
-  match ga, gb with
-    (_, _), (_, []) -> failwith("Nique ta mere")
-  | (a, b), (c, d) -> not a = c, diviser_multiple_abs b d
+let diviser_multiple ((a, _) as ga) ((c, _) as gb) =
+  let _, (s, div) = div_eucl (ga, gb) in
+  if a then
+    additionner (a <> c, div) (not c, [1])
+  else
+    s, div
 
-let modulo ga gb =
-  match ga, gb with
-    (_, _), (_, []) -> failwith("Ah")
-  | (_ (* a *), b), (_ (* c *), d) ->
-    let rec modulo_rec i j =
-      if comparer_nbr_abs i j = -1 then
-        modulo_rec (sous(i, j)) j
-      else
-        i
-    in modulo_rec b d
+(** renvoie le reste de la division euclidienne de ga par gb *)
+let modulo ((a, _) as ga) ((_, d) as gb) =
+  let (s, m), _ = div_eucl (ga, gb) in
+  if a then
+    additionner (a, m) (false, d)
+  else
+    s, m
+
+(** renvoie pgcd(ga, gb) *)
+let pgcd _ _ = (false, [])
 
 (** renvoie (nominateur, dénominateur) de la fraction ga / gb *)
 let diviser _ _ = ((false, []), (false, []))
@@ -187,7 +207,7 @@ let cse_rec a n =
     match i with
       i when i = n - 1 -> []
     | _ -> (int_of_char a.[i]) - 48 :: abc a (i - 1)
-  in abc a (String.length a - 1)
+  in abc a (String.length a - 1) |> nettoyer_zero
 
 let grandentier_depuis_texte sa =
   if sa.[0] = '-' then
@@ -200,15 +220,15 @@ let grandentier_depuis_texte sa =
 
 (*Convertit basiquement le nombre*)
 let textedechiffre ga =
-  let rec tdc = function
-      [] -> ""
-    | e :: ga -> tdc ga ^ (string_of_int e)
-  in tdc ga
+  let rec tdc acc = function
+      [] -> acc
+    | e :: ga -> (tdc [@tailcall]) ((string_of_int e) ^ acc) ga
+  in tdc "" ga
 
 (** renvoie la représentation textuelle d'un grandentier *)
 let texte_depuis_grandentier ga =
-  let (a, b) = ga in
-  if a = true then
+  let a, b = ga in
+  if a then
     "-" ^ textedechiffre b
   else
     textedechiffre b

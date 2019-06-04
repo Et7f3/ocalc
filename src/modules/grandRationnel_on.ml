a060d89da24e387ca58087b84f8ebe11bff43031
Élie BRAMI <elie.brami@epita.fr>
2019-06-04 17:51:36 +0200

    remove comment

--- src/modules/grandRationnel_on.ml
+++ src/modules/grandRationnel_on.ml
@@ -2,14 +2,6 @@ open GrandEntier_on
 
 type grandrationnel = bool * int list * int list
 
-(*
-let additionerq (a, b, c) (d, e, f) =
-  let z, elie, travail = bigint_times b f, bigint_times e c, bigint_times c f in
-  let alex, gautier = additioner (a, z) (d, elie) in
-  let quentin, tropdeu = diviser (false, gautier) (false, travail) in
-  alex, quentin, tropdeu
-*)
-
 let additioner (a, b, c) (d, e, f) =
   let z, elie, travail = multiplier (a, b) (false, f), multiplier (false, c) (d, e), multiplier (false, c) (false, f) in
   let alex, gautier = additioner z elie in

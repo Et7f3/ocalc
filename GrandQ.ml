f45e40bd61ba924ed52b199303f9575108196d96
Élie BRAMI <elie.brami@epita.fr>
2019-06-04 11:06:59 +0200

    coding style of GrandQ.ml

--- GrandQ.ml
+++ GrandQ.ml
@@ -2,25 +2,25 @@ type grandq = bool * int list * int list
 
 
 
-let additionnerq (a,b,c) (d,e,f) =
-  let z,elie,travail = bigint_times b f, bigint_times e c,bigint_times c f in
-  let (alex,gautier) = additionner (a,z) (d,elie) in
-  let quentin,tropdeu = diviser (false,gautier) (false,travail) in
-  alex,quentin,tropdeu
+let additionnerq (a, b, c) (d, e, f) =
+  let z, elie, travail = bigint_times b f, bigint_times e c, bigint_times c f in
+  let alex, gautier = additionner (a, z) (d, elie) in
+  let quentin, tropdeu = diviser (false, gautier) (false, travail) in
+  alex, quentin, tropdeu
 
-let soustractionq (a,b,c) (d,e,f) =
-  let z,elie,travail = bigint_times b f, bigint_times e c,bigint_times c f in
-  let (alex,gautier) = soustraire (a,z) (d,elie) in
-  let quentin,tropdeu = diviser (false,gautier) (false,travail) in
-  alex,quentin,tropdeu
+let soustractionq (a, b, c) (d, e, f) =
+  let z, elie, travail = bigint_times b f, bigint_times e c, bigint_times c f in
+  let alex, gautier = soustraire (a, z) (d, elie) in
+  let quentin, tropdeu = diviser (false, gautier) (false, travail) in
+  alex, quentin, tropdeu
 
-let soustractionq (a,b,c) (d,e,f) =
-  additionnerq (a,b,c) (not d,e,f)
+let soustractionq (a, b, c) (d, e, f) =
+  additionnerq (a, b, c) (not d, e, f)
 
-    
-let multiplierq (a,b,c) (d,e,f) =
-  let z,y = bigint_times b e , bigint_times c f in
-  let x,i = diviser (false,z) (false,y) in
-  (a != d), x , i;;
 
-let diviserq (a,b,c) (d,e,f) = multiplierq (a,b,c) (d,f,e);;
+let multiplierq (a, b, c) (d, e, f) =
+  let z, y = bigint_times b e, bigint_times c f in
+  let x, i = diviser (false, z) (false, y) in
+  a != d, x, i
+
+let diviserq (a, b, c) (d, e, f) = multiplierq (a, b, c) (d, f, e)

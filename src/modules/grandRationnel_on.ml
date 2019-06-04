0921a6f18e6c8db84b7378d0acb33fb22c43283f
Élie BRAMI <elie.brami@epita.fr>
2019-06-04 22:35:17 +0200

    remove comment

--- src/modules/grandRationnel_on.ml
+++ src/modules/grandRationnel_on.ml
@@ -2,16 +2,10 @@ open GrandEntier_on
 
 type grandrationnel = bool * int list * int list
 
-(*
-let additionerq (a, b, c) (d, e, f) =
-  let z, elie, travail = bigint_times b f, bigint_times e c, bigint_times c f in
-  let alex, gautier = additioner (a, z) (d, elie) in
-  let quentin, tropdeu = diviser (false, gautier) (false, travail) in
-  alex, quentin, tropdeu
-*)
-
 let additioner (a, b, c) (d, e, f) =
-  let z, elie, travail = multiplier (a, b) (false, f), multiplier (false, c) (d, e), multiplier (false, c) (false, f) in
+  let z = multiplier (a, b) (false, f)
+  and elie = multiplier (false, c) (d, e)
+  and travail = multiplier (false, c) (false, f) in
   let alex, gautier = additioner z elie in
   let (_, quentin), (_, tropdeu) = diviser (alex, gautier) travail in
   alex, quentin, tropdeu
@@ -20,7 +14,8 @@ let soustraire (a, b, c) (d, e, f) =
   additioner (a, b, c) (not d, e, f)
 
 let multiplier (a, b, c) (d, e, f) =
-  let thomas, mouton = multiplier (false, b) (false, e), multiplier (false, c) (false, f) in
+  let thomas = multiplier (false, b) (false, e)
+  and mouton = multiplier (false, c) (false, f) in
   let (_,x), (_,i) = diviser thomas mouton in
   a <> d, x, i
 

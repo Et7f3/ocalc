dfa5b87ecded323f958703c1ee88bb1be5741f1b
Élie BRAMI <elie.brami@epita.fr>
2019-06-04 22:32:35 +0200

    light coding style :thumbsup:

--- src/modules/grandNum_on.ml
+++ src/modules/grandNum_on.ml
@@ -4,30 +4,27 @@ type num =
   | Q of GrandRationnel_on.grandrationnel
 
 let powerabs c =
-  let rec pa i c = match c with
+  let rec pa i = function
       0 -> i
-    | _ -> pa (0 :: i) (c-1)
+    | c -> pa (0 :: i) (c - 1)
   in pa [1] (abs c)
 
 let rec up (a, b, c) =
   if c <= 0 then
-    (a, b, [1])
+    a, b, [1]
   else
     up (a, 0 :: b, c - 1)
 
-
 let truc (a, b, c) =
   if c = 0 then
-    (a, b, [1])
+    a, b, [1]
   else if c > 0 then
     up (a, b, c)
   else
     a, b, powerabs c
 
-
-
 let multiplier = function
-  E e1, E e2 -> E (GrandEntier_on.multiplier e1 e2)
+    E e1, E e2 -> E (GrandEntier_on.multiplier e1 e2)
   | R r1, R r2 -> R (GrandReel_on.multiplier r1 r2)
   | Q q1, Q q2 -> Q (GrandRationnel_on.multiplier q1 q2)
   | R r2, E (signe, e1) -> R (GrandReel_on.multiplier (signe, e1, 0) r2)
@@ -36,7 +33,6 @@ let multiplier = function
   | Q q1, E (signe, e1) -> Q (GrandRationnel_on.multiplier q1 (signe, e1, [1]))
   | R r1, Q q1 -> Q (GrandRationnel_on.multiplier q1 (truc r1))
   | Q q1, R r1 -> Q (GrandRationnel_on.multiplier q1 (truc r1))
-
 
 let soustraire = function
     E e1, E e2 -> E (GrandEntier_on.soustraire e1 e2)
@@ -49,7 +45,6 @@ let soustraire = function
   | R r1, Q q1 -> Q (GrandRationnel_on.soustraire  q1 (truc r1))
   | Q q1, R r1 -> Q (GrandRationnel_on.soustraire  q1 (truc r1))
 
-
 let additioner = function
     E e1, E e2 -> E (GrandEntier_on.additioner e1 e2)
   | R r1, R r2 -> R (GrandReel_on.additioner r1 r2)
@@ -61,10 +56,8 @@ let additioner = function
   | R r1, Q q1 -> Q (GrandRationnel_on.additioner q1 (truc r1))
   | Q q1, R r1 -> Q (GrandRationnel_on.additioner q1 (truc r1))
 
-
-
 let diviser = function
-    E (signe1, e1), E (signe2, e2) -> Q (signe1 != signe2, e1, e2)
+    E (signe1, e1), E (signe2, e2) -> Q (signe1 <> signe2, e1, e2)
   | R r1, R r2 -> Q (GrandRationnel_on.diviser (truc r1) (truc r2))
   | Q q1, Q q2 -> Q (GrandRationnel_on.diviser q1 q2)
   | R r2, E (signe, e1) -> Q (GrandRationnel_on.diviser (signe, e1, [1]) (truc r2))
@@ -74,22 +67,26 @@ let diviser = function
   | R r1, Q q1 -> Q (GrandRationnel_on.diviser q1 (truc r1))
   | Q q1, R r1 -> Q (GrandRationnel_on.diviser q1 (truc r1))
 
-
-let opposer = function 
-	  E (signe, e1) -> E (not signe, e1)
-	| Q (signe, q1, q2) -> Q (not signe, q1, q2)
-	| R (signe, r, exp) -> R (not signe, r, exp)
+let opposer = function
+    E (signe, e1) -> E (not signe, e1)
+  | Q (signe, q1, q2) -> Q (not signe, q1, q2)
+  | R (signe, r, exp) -> R (not signe, r, exp)
 
 let inverser = function
-	  E (signe, e1) ->  Q (signe, [1], e1)
-	| Q (signe, q1, q2) -> Q  (signe, q2, q1)
-	| R r1 -> let (a, b, c) = truc r1 in Q (a, c, b)
+    E (signe, e1) -> Q (signe, [1], e1)
+  | Q (signe, q1, q2) -> Q (signe, q2, q1)
+  | R r1 ->
+    let a, b, c = truc r1 in
+    Q (a, c, b)
+
 let texte_depuis_num = function
-	  E e1 ->  GrandEntier_on.texte_depuis_grandentier e1
-	| R r1 -> GrandReel_on.texte_depuis_grandreel r1
-	| Q (signe, q1, q2) -> 
-	if signe then 
-	"-" ^ GrandEntier_on.texte_depuis_grandentier (false, q1) ^ "/" ^ GrandEntier_on.texte_depuis_grandentier (false, q2) 
-	else 
-	""  ^ GrandEntier_on.texte_depuis_grandentier (false, q1) ^ "/" ^ GrandEntier_on.texte_depuis_grandentier (false, q2)
-	
+    E e1 ->  GrandEntier_on.texte_depuis_grandentier e1
+  | R r1 -> GrandReel_on.texte_depuis_grandreel r1
+  | Q (signe, q1, q2) ->
+    let q1 = GrandEntier_on.texte_depuis_grandentier (false, q1)
+    and q2 = GrandEntier_on.texte_depuis_grandentier (false, q2) in
+    let q = q1 ^ "/" ^ q2 in
+    if signe then
+      "-" ^ q
+    else
+      q

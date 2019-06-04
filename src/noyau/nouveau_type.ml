4838ef8973f07df0f259ee07126023c48b59fc5d
Élie BRAMI <elie.brami@epita.fr>
2019-06-04 10:27:09 +0200

    add support for constants and quaternion

--- src/noyau/nouveau_type.ml
+++ src/noyau/nouveau_type.ml
@@ -1,8 +1,16 @@
 open Lien
 
+type constantes =
+    Pi
+    (* Quaternion constantes*)
+  | I
+  | J
+  | K
+
 type expr =
     E of GrandEntier.grandentier
   | R of GrandReel.grandreel
+  | C of constantes
   | Var of string
   | T of int * expr list
   | Fx of string * int * expr list
@@ -16,6 +24,10 @@ let rec expr_depuis_expression =
     Vide -> failwith "impossible de convertir Vide"
   | Entier s -> E (GrandEntier.grandentier_depuis_texte s)
   | Reel s -> R (GrandReel.grandreel_depuis_texte s)
+  | Variable ("pi" | "pI" | "Pi" | "PI") -> C Pi
+  | Variable ("i" | "I") -> C I
+  | Variable ("j" | "J") -> C J
+  | Variable ("k" | "K") -> C K
   | Variable s -> Var s
   | Tuple (n, l) -> T (n, List.map expr_depuis_expression l)
   | Fonction (s, n, l) -> Fx (s, n, List.map expr_depuis_expression l)

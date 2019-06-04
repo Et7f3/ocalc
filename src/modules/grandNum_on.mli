c8b9d0aefaff204a0e0f3f87bc248f2e1318a9fc
rivar59 <46228238+rivar59@users.noreply.github.com>
2019-06-04 21:49:48 +0200

    Update grandNum_on.mli

--- src/modules/grandNum_on.mli
+++ src/modules/grandNum_on.mli
@@ -2,12 +2,12 @@ type num =
     E of GrandEntier_on.grandentier
   | R of GrandReel_on.grandreel
   | Q of GrandRationnel_on.grandrationnel
-val powerabs : int -> int list = <fun>
-val up : 'a * int list * int -> 'a * int list * int list = <fun>
-val truc : 'a * int list * int -> 'a * int list * int list = <fun>
-val multiplier : num * num -> num = <fun>
-val soustraire : num * num -> num = <fun>
-val additioner : num * num -> num = <fun>
-val diviser : num * num -> num = <fun>
-val opposer : num -> num = <fun>
-val inverser : num -> num = <fun>
+val powerabs : int -> int list
+val up : 'a * int list * int -> 'a * int list * int list
+val truc : 'a * int list * int -> 'a * int list * int list
+val multiplier : num * num -> num
+val soustraire : num * num -> num
+val additioner : num * num -> num
+val diviser : num * num -> num
+val opposer : num -> num
+val inverser : num -> num
